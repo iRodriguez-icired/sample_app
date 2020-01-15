46be4a6060471a66703cba6639a5ef829ba6b4a0
iRodriguez-icired <irodriguez@iciredimpagados.com>
2020-01-15 09:29:35 +0000

    Add login after signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,20 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { 
-        user: {
-          name: "",
-          email: "user@invalid",
-          password: "foo",
-          password_confirmation: "bar"
-        }
-      } 
-    end
-    assert_template 'users/green'
-  end
+  
   
   test "valid signup information" do
     get signup_path
@@ -29,7 +16,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
       follow_redirect!
       assert_template 'users/show'
-        
+      assert is_logged_in?
     end
   end
 end
