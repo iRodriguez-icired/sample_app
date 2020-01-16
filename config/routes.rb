30a8d6a6f30e875bbb4d79933b2f7d5635acf36c
iRodriguez-icired <irodriguez@iciredimpagados.com>
2020-01-16 11:36:06 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'users/new'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -11,4 +13,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
