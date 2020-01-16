2539cf275834db9d9e954f979c262921fba55114
iRodriguez-icired <irodriguez@iciredimpagados.com>
2020-01-16 10:09:15 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,9 +20,10 @@ class UsersController < ApplicationController
     
       @user = User.new(user_params)
       if(@user.save)
+        @user.send_activation_email
+        flash[:info] = "Please check your email to activate your account."
+        redirect_to root_url
         log_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
       else
         render 'new'
       end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,9 +14,7 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   
-  def log_in_as(user)
-    session[:user_id] = user.id
-  end
+  
   
   
 end
